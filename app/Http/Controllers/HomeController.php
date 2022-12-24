5114e134fe01c2ef56fe735d934c12a617cbdeff
haider-cse <haider.cse7644@gmail.com>
2022-12-24 10:21:52 -0600

    save info in database

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -20,58 +20,58 @@ class HomeController extends Controller
         $flights = $all_flights['flightOffer'];
         // dd($flights[0]);
 
-        foreach ($flights as $key => $flight) {
+        foreach ($flights as $flight) {
+
             $flight_model = new Flight();
             $flight_model->price = $flight['price'];
             $flight_model->save();
-           dd($flight);
-           
-            foreach ($flight as $key => $f_val) {
-                dump($f_val);
-                foreach ($f_val as $key => $value_itineraries) {
-                    $flight_itineraries = new Itinerarie();
-                    $flight_itineraries->flight_id = $flight_model->id;
-                    $flight_itineraries->duration = $value_itineraries['duration'];
-                    $flight_itineraries->save();
 
-                    foreach ($value_itineraries['segments'] as $key => $segment) {
-                        $segment_model = new Segment();
-                        $segment_model->itineraries_id =  $flight_itineraries->id;
-                        $segment_model->marketingCarrier = $segment['marketingCarrier'];
-                        $segment_model->carrierCode =  $segment['carrierCode'];
-                        $segment_model->flightNumber =  $segment['flightNumber'];
-                        $segment_model->aircraft =  $segment['aircraft'];
-                        $segment_model->save();
-                        foreach ($segment['departure'] as $key => $value_departure) {
-                            $departure = new Departure();
-                            $departure->segment_id = $segment_model->id;
-                            $departure->iataCode = $segment['departure']['iataCode'];
-                            $departure->at = $segment['departure']['at'];
-                            $departure->save();
-                        }
+            foreach ($flight['itineraries'] as $flight_itineraries) {
+                $itineraries = new Itinerarie();
+                $itineraries->flight_id = $flight_model->id;
+                $itineraries->duration = $flight_itineraries['duration'];
+                $itineraries->save();
 
-                        foreach ($segment['arrival'] as $key => $value_arrival) {
-                            $arrival = new Arrival();
-                            $arrival->segment_id = $segment_model->id;
-                            $arrival->iataCode = $segment['arrival']['iataCode'];
-                            $arrival->at = $segment['arrival']['at'];
-                            $arrival->save();
-                        }
+                foreach ($flight_itineraries['segments'] as $key => $segment) {
+
+                    $segment_model = new Segment();
+                    $segment_model->itineraries_id = $itineraries->id;
+                    $segment_model->marketingCarrier = $segment['marketingCarrier'];
+                    $segment_model->carrierCode = $segment['carrierCode'];
+                    $segment_model->flightNumber = $segment['flightNumber'];
+                    $segment_model->aircraft = $segment['aircraft'];
+                    $segment_model->save();
+
+                    foreach ($segment['departure'] as $key => $departure) {
+                        $departure = new Departure();
+                        $departure->segment_id = $segment_model->id;
+                        $departure->iataCode = $segment['departure']['iataCode'];
+                        $departure->at = $segment['departure']['at'];
+                        $departure->save();
+                    }
+                    foreach ($segment['arrival'] as $key => $arrival) {
+                        $arrival = new Arrival();
+                        $arrival->segment_id = $segment_model->id;
+                        $arrival->iataCode = $segment['arrival']['iataCode'];
+                        $arrival->at = $segment['arrival']['at'];
+                        $arrival->save();
                     }
                 }
             }
 
-            foreach ($flight['fareBasis'] as $key => $f_val) {
-                foreach ($f_val as $f_basis) {
+            foreach ($flight['fareBasis'] as $key => $fareBasis) {
+
+                foreach ($fareBasis as $f_basis) {
                     $fareBasis = new FareBasis();
                     $fareBasis->flight_id = $flight_model->id;
                     $fareBasis->name = $f_basis;
                     $fareBasis->save();
                 }
             }
+
             foreach ($flight['class'] as $key => $f_val) {
                 foreach ($f_val as $f_class) {
-                    $classes = new FareBasis();
+                    $classes = new FlightClass();
                     $classes->flight_id = $flight_model->id;
                     $classes->name = $f_class;
                     $classes->save();
@@ -79,14 +79,14 @@ class HomeController extends Controller
             }
             foreach ($flight['seat'] as $key => $f_val) {
                 foreach ($f_val as $f_seat) {
-                    $seat = new FareBasis();
+                    $seat = new Seat();
                     $seat->flight_id = $flight_model->id;
                     $seat->seat_number = $f_seat;
                     $seat->save();
                 }
             }
         }
-        dd('OK');
+
         return view('home');
     }
     public function store(Request $request)
